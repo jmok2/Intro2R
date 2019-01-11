df52ea8c813ba44ee6c6e7f50dcf52432aa6c4d5
Eric Archer <eric.archer@noaa.gov>
2019-01-11 14:45:14 -0800

    week 1 update

---
+++ Class 1 Examples/first script.R
@@ -0,0 +1,13 @@
+5 + 6
+
+example(sd)
+
+# this is an example of how to get help for the mean function
+# second line example of comment
+help(mean)
+
+#' this is the first line
+#' here's my second line
+#' and my third
+
+
